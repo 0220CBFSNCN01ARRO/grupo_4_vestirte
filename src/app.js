e961a766c8a255e2d698ec769031aafe52fdff08
Iasi Salomon <iasisalomon@gmail.com>
2020-05-28 12:23:57 -0300

    cambios en admin prod

--- src/app.js
+++ src/app.js
@@ -34,7 +34,6 @@ app.use ('/usuarios', usuariosRouter);
 app.use ('/carrito', carritoRouter);
 
 //***OTHER***//
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
