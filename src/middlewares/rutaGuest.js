0a99cfb2f174732d6911635c65f647c4fab2cfbe
Iasi Salomon <iasisalomon@gmail.com>
2020-06-25 15:11:40 -0300

    arreglado middleware de guest

--- src/middlewares/rutaGuest.js
+++ src/middlewares/rutaGuest.js
@@ -3,13 +3,9 @@ const db = require ('../database/models');
 
 module.exports = (req, res, next) => {
     if (req.session.user) {
-        db.usuarios.findByPk(req.session.user.id)
-        .then (function(usuariolog){
-            console.log (usuariolog.dataValues)
-            res.redirect(`perfil/${usuariolog.dataValues.id}`)
+            res.redirect(`perfil/${req.session.user.id}`)
             return;
         }
-        )}
         next ()
     }
     
