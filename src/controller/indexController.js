638844d5e6f5c61fdcafe387fa4c3e11977ae1c9
Iasi Salomon <iasisalomon@gmail.com>
2020-07-02 11:29:05 -0300

    bugfix visitados con mas hardcode

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -1,6 +1,7 @@
 const db = require ('../database/models')
 const { sequelize } = require('../database/models')
 
+
 module.exports = {
     root: async (req, res) => {
         let destacados = await db.productos.findAll({
@@ -9,16 +10,13 @@ module.exports = {
             }
         })
 
+        if (res.locals.user){pendorcho=res.locals.user.id} else {pendorcho=null}
+        
         let visitados = await db.sequelize.query (`SELECT * FROM usuario_producto, usuarios, productos WHERE usuarioId = usuarios.id
         AND productoId = productos.id
-        AND usuario_producto.usuarioId = ${res.locals.user.id}`
+        AND usuario_producto.usuarioId = ${pendorcho}`
         )
 
         return res.render('index', {destacados, visitados})
     },
 }
-
-
-
-
-
