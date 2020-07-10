a918990095aa28c4e978d3bc86630b625627e614
peduito <peduito@gmail.com>
2020-07-10 10:54:55 -0300

    casi anda

--- public/js/validacionprodcutoscrear.js
+++ public/js/validacionprodcutoscrear.js
@@ -1,23 +1,30 @@
-const formulario = document.getElementById('formulario');
-const nombre = document.getElementById('nombre');
+window.addEventListener('load', function () {
+    let formulario = document.getElementById('formulario');
+    let nombre = document.getElementById('nombre');
+
+    formulario.addEventListener('submit', () => {
+   
+
+       /*  if (nombre.value == '') {
+           formulario.querySelector('label').innerHTML = 'Samu, estas aca!'
+        } */
 
 
+        let errores = [];
 
-formulario.addEventListener('submit', (e)=>{
+        if (nombre.value == '') {
+            errores.push('completa el campo de nombre')
+        } else if (nombre.value.length < 3) {
+            errores.push('Debe tener al menos 3 caracteres')
+        };
+        if (errores.length > 0) {
+            evento.preventDefault();
+            let ulErrores = document.querySelector('div.errores ul')
+            for (let index = 0; index < errores.length; index++) {
 
-    let errores = [];
+                ulErrores.innerHTML = '<li>' + errores[index] + '</li>'
 
-    if (nombre.value == ''){
-        alert('completa el campo de nombre')
-    }
-    if (errores.length > 0){
-        e.preventDefault();
-
-        let ulErrores = document.querySelector ('div.errores ul')
-        for (let index = 0; index < errores.length; index++) {
-
-            ulerrores.innerHTML += '<li>'+ errores[index] + '</li>'
+            }
         }
-    }
-
-});
+    })
+})
