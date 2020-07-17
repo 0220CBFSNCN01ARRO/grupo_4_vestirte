b6d1ed60d726a10fafef57aff1874f9c8cf1fa87
PabloCane <61993584+PabloCane@users.noreply.github.com>
2020-07-16 22:35:52 -0300

    back de editar producto sin imagen

--- src/controller/productosController.js
+++ src/controller/productosController.js
@@ -1,95 +1,114 @@
 //***MODULE REQUIRE***//
-const db = require ('../database/models')
-const {validationResult} = require('express-validator')
+const db = require('../database/models')
+const {
+    check,
+    validationResult,
+    body
+} = require('express-validator');
 
 //***CONTROLLER METHODS***//
 
-module.exports= {
-crear: (req, res) => {
-    db.productos.findAll()
-    .then (function(productos){
-        res.render('productos-crear', {productos});
-    })
-},
+module.exports = {
+    crear: (req, res) => {
+        db.productos.findAll()
+            .then(function (productos) {
+                res.render('productos-crear', {
+                    productos
+                });
+            })
+    },
 
-listar: (req, res) => {
-    db.productos.findAll()
-    .then (function(productos){
-        res.render('productos', {productos})
-    })
-},
+    listar: (req, res) => {
+        db.productos.findAll()
+            .then(function (productos) {
+                res.render('productos', {
+                    productos
+                })
+            })
+    },
 
-detalle: async (req, res) => {
-         let producto= await db.productos.findByPk(req.params.productoId)
-
-        
-         return res.render('productos-detalle', {producto});
-        } ,
+    detalle: async (req, res) => {
+        let producto = await db.productos.findByPk(req.params.productoId)
 
 
-guardar:  (req, res) => {
-   
-    let errors=validationResult(req);
-    console.log(errors)
-    if (errors.isEmpty()){
-
-    
-      db.productos.create ({
-        nombre:req.body.nombre,
-        descripcion:req.body.descripcion,
-        precio:req.body.precio,
-        descuento:req.body.descuento,
-        imagen:req.files[0].filename,
-        categoria:req.body.categoria,
-        envio:req.body.envio,
-        stock:req.body.stock,
+        return res.render('productos-detalle', {
+            producto
         });
-    res.redirect('/productos')
-}else{
-    
-    let errores= errors.errors
-    
-     res.send('ERROR')
-     
-
-}
-}
+    },
 
 
-,
+    guardar: (req, res) => {
 
-editar: (req, res) => {
-   
-    db.productos.findByPk(req.params.productoId)
-    .then (function(productoedit){
-        console.log (productoedit.dataValues)
-        res.render('productos-editar',{productoedit})
-    })
-},
+            let errors = validationResult(req);
+            console.log(errors)
+            if (errors.isEmpty()) {
 
-actualizar: (req, res) => {
-    db.productos.update ({
-        nombre:req.body.nombre,
-        descripcion:req.body.descripcion,
-        precio:req.body.precio,
-        descuento:req.body.descuento,
-        imagen:req.body.imagen,
-        categoria:req.body.categoria,
-        envio:req.body.envio,
-        stock:req.body.stock,
-        }, { 
-            where: {
-            id: req.params.productoId
-        }        
-        });
-    res.redirect('/productos/detalles/' + req.params.productoId)
-},
-destruir : (req, res) => {
-    db.productos.destroy({
-        where:{
-            id:req.params.productoId
+
+                db.productos.create({
+                    nombre: req.body.nombre,
+                    descripcion: req.body.descripcion,
+                    precio: req.body.precio,
+                    descuento: req.body.descuento,
+                    imagen: req.files[0].filename,
+                    categoria: req.body.categoria,
+                    envio: req.body.envio,
+                    stock: req.body.stock,
+                });
+                res.redirect('/productos')
+            } else {
+
+                let errores = errors.errors
+
+                res.send('ERROR')
+
+
+            }
         }
-    })
-    res.redirect('/productos/crear')
-}
+
+
+        ,
+
+    editar: (req, res) => {
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+
+            db.productos.findByPk(req.params.productoId)
+                .then(function (productoedit) {
+                    console.log(productoedit.dataValues)
+                    res.render('productos-editar', {
+                        productoedit
+                    })
+                })
+        } else {
+            return res.render('productos-editar', {
+                errors: errors.errors
+            })
+        }
+    },
+
+    actualizar: (req, res) => {
+        db.productos.update({
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            descuento: req.body.descuento,
+            imagen: req.body.imagen,
+            categoria: req.body.categoria,
+            envio: req.body.envio,
+            stock: req.body.stock,
+        }, {
+            where: {
+                id: req.params.productoId
+            }
+        });
+        res.redirect('/productos/detalles/' + req.params.productoId)
+    },
+    destruir: (req, res) => {
+        db.productos.destroy({
+            where: {
+                id: req.params.productoId
+            }
+        })
+        res.redirect('/productos/crear')
+    }
 };
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,8 +1,8 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-const multer = require ('multer');
-let path = require ('path');
+const multer = require('multer');
+let path = require('path');
 
 const crearValidator = require('../middlewares/creacionProductosValidator')
 
@@ -11,36 +11,50 @@ const crearValidator = require('../middlewares/creacionProductosValidator')
 
 // ************ Multer************
 const storage = multer.diskStorage({
-    destination: function (req, file, cb){
+    destination: function (req, file, cb) {
         cb(null, 'public/img/img-productos')
     },
-    filename: function (req, file, cb){
+    filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     },
 });
-var upload = multer ({storage: storage});
+var upload = multer({
+    storage: storage
+});
 
 
 
 
 // ************ Require's Controllers************
-const productosController = require('../controller/productosController.js')
+const productosController = require('../controller/productosController.js');
+const {
+    check
+} = require('express-validator');
 
-/*** TODOS LOS PRODUCTOS ***/ 
+/*** TODOS LOS PRODUCTOS ***/
 router.get('/', productosController.listar);
 
-/*** DETALLE DE PRODUCTO ***/ 
+/*** DETALLE DE PRODUCTO ***/
 router.get('/detalles/:productoId/', productosController.detalle);
 
-/*** CREAR UN PRODUCTO ***/ 
+/*** CREAR UN PRODUCTO ***/
 router.get('/crear/', productosController.crear);
-router.post('/crear/',upload.any(),crearValidator.checkProduct,productosController.guardar);
+router.post('/crear/', upload.any(), crearValidator.checkProduct, productosController.guardar);
 
-/*** EDITAR UN PRODUCTO ***/ 
+/*** EDITAR UN PRODUCTO ***/
 router.get('/editar/:productoId', productosController.editar);
-router.put('/:productoId/', productosController.actualizar);
+router.put('/:productoId/',
+    [
+        check('nombre').isLength({
+            min: 5
+        }).withMessage('Nombre debe tener 5 caracteres.'),
+        check('descripcion').isLength({
+            min: 20
+        }).withMessage('La descripcion debe tener al menos 20 caracteres.')
+    ],
+    productosController.actualizar);
 
-/*** ELIMINAR PRODUCTO***/ 
+/*** ELIMINAR PRODUCTO***/
 router.delete('/:productoId/destruir', productosController.destruir);
 
 module.exports = router;
