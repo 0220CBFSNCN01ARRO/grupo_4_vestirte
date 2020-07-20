7e41437ebd624a91b38d2136a3704a8aaad5ef92
Iasi Salomon <iasisalomon@gmail.com>
2020-07-20 02:22:16 -0300

    guapeson

--- public/js/login.js
+++ public/js/login.js
@@ -10,16 +10,18 @@
 
     email.addEventListener ('change', e => {
         if (email.value.length < 4 ) {
-            if (!error) {
+            if (error != 'a') {
             let errormail=document.getElementById('errormail');
+            error = "a";
             return errormail.innerHTML += `<p >Formato incorrecto para mail</p>`}
             }
         })
 
     password.addEventListener ('change', e => {
         if (password.value.length < 3) {
-            if (!error) {
+            if (error != 'b') {
                 let errorpass=document.getElementById('errorpass');
+                error = "b"
                 return errorpass.innerHTML = `<p >Contraseña muy corta </p>`}
     }
     
