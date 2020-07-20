ad22ad00c78541a181d3fd5e223cfe763e815945
Iasi Salomon <iasisalomon@gmail.com>
2020-07-20 17:19:57 -0300

    validacion front register

--- public/js/validacionRegister.js
+++ public/js/validacionRegister.js
@@ -1,44 +1,68 @@
-const nombre = document.getElementById('nombre');
-const apellido = document.getElementById('apellido');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const image = document.getElementById('image');
-const formulario = document.getElementById('formulario');
+ window.addEventListener('load', function() { 
+
+
+
+
+
+
 const errorTex = document.getElementById('errorTex');
-const regular = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
 
-formulario.addEventListener('submit', e => {
-     errorTex.innerHTML = "";
-     let error = "";
+
+
+
+
+     nombre.addEventListener ('change', () => {
+        const nombre = document.getElementById('nombre');
      if (nombre.value.length < 3 || !isNaN(nombre.value)) {
-          error += `<p class="error">Formato incorrecto para nombre </p>`
-          entrar = true;
+        document.getElementById('nombreVal');
+        nombreVal.innerHTML = `<p class="error">Formato incorrecto para nombre </p>`
+         
+     }else{
+        nombreVal.innerHTML = ``
      }
-     if (apellido.value.length < 3 || !isNaN(apellido.value)) {
-          error += `<p class="error">Formato incorrecto para apellido </p>`
-          entrar = true;
-     }
-     if (regular.test(email.value) === false) {
-          /* alert('email no valido')} */
-          error += `<p class="error">El email no es valido </p>`
-          entrar = true;
-     }
-     if (password.value.length < 7) {
-          error += `<p class="error" >password minimo 8 caracteres </p>`
-          entrar = true;
-     }
-     if (image.value === "") {
-          error += `<p class="error">no mando imagen </p><br>`
-          entrar = true;
-     }
-     if (entrar) {
-          e.preventDefault();
-          errorTex.innerHTML += error
+    })
 
-          error=false;
+
+    apellido.addEventListener ('change',(e) => {
+
+        const apellido = document.getElementById('apellido');
+     if (apellido.value.length < 3 || !isNaN(apellido.value)) {   
+        document.getElementById('apellidoVal');
+        apellidoVal.innerHTML = `<p class="error">Formato incorrecto para apellido </p>`
+         
+     }else{
+         apellidoVal.innerHTML = ``
      }
-})
+     
+    }) 
+
+       email.addEventListener ('change', () => {
+        const email = document.getElementById('email');
+        const regular = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+     if (regular.test(email.value) === false) {
+        document.getElementById('emailVal');
+        emailVal.innerHTML = `<p class="error">El email no es valido </p>`
+         
+     }
+     else{
+        emailVal.innerHTML = ``
+     }
+    }) 
+
+    password.addEventListener ('change', () => {
+        const password = document.getElementById('password');
+     if (password.value.length < 7) {
+        document.getElementById('passVal');
+        passVal.innerHTML = `<p class="error" >password minimo 8 caracteres </p>`
+          
+     }else{
+        passVal.innerHTML = ''
+     }
+    })
+
+    
+
 
 //funcion para validar imagenen
 //Los criterios a validar serán
@@ -67,4 +91,20 @@ function validarImagen(obj) {
 
 
 }
-//quitar texto por defecto file
+const formulario = document.getElementById('formulario');
+formulario.addEventListener ('submit', (e) => {
+    
+   
+        const image = document.getElementById('image');
+     if (image.value === "" || password.value===""|| email.value===""||apellido.value===""||nombre.value==="") {
+        e.preventDefault()
+        errorTex.innerHTML = `<p class="error">Todos lo campos son obligatorios </p><br>`
+         
+     }else{
+        formulario.addEventListener ('submit', () => {
+            errorTex.innerHTML = ``
+     })
+    }
+        
+})
+ }) 
