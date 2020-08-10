0d3c1aa759439536066f1a7c9b115de3d0456dbc
Iasi Salomon <iasisalomon@gmail.com>
2020-08-10 15:21:45 -0300

    carrito

--- src/controller/carritoController.js
+++ src/controller/carritoController.js
@@ -6,9 +6,9 @@ carrito: async (req, res, next) =>{
     let productos = [];
     let qty = [];
 
-    if (req.body.juy){
-    
-    pabuscar = JSON.parse(req.body.juy)
+    if (req.body.juy || req.body.car){
+    if (req.body.juy) {pabuscar = JSON.parse(req.body.juy)}
+    if (req.body.car) {pabuscar = JSON.parse(req.body.car)}
     for (prod of pabuscar){
     let prodyg = {};
     let prody =  await db.productos.findByPk(prod.id)
